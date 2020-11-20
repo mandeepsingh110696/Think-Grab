5d3d69fd62fae3dce034a5353a3d8cf4fe1a396a
unknown <manhack7897@gmail.com>
2020-11-20 01:38:43 -0500

    signup updated with firebase auth and firbase firestore

--- Signup.java
+++ Signup.java
@@ -4,22 +4,38 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
+
+//signup updated
 public class Signup extends AppCompatActivity {
 
     Button bt1;
     EditText email,name,pass,confpass;
+    ProgressBar progressBar;
+    TextView already_signupp;
+
 	//FirebaseFirestore instance 
     FirebaseFirestore firebaseFirestoredb;
+    FirebaseAuth firebaseAuth;
 
 
     @Override
@@ -31,46 +47,131 @@ public class Signup extends AppCompatActivity {
         name=findViewById(R.id.name_signup);
         pass=findViewById(R.id.pass_signup);
         confpass=findViewById(R.id.conf_pass_signup);
+        progressBar=findViewById(R.id.progressBar);
+        already_signupp= findViewById(R.id.already_signup);
+
         firebaseFirestoredb=FirebaseFirestore.getInstance();
+        firebaseAuth=FirebaseAuth.getInstance();
 
 
+
+         if(firebaseAuth.getCurrentUser() !=null)
+         {
+             Intent in = new Intent(Signup.this,Navigation.class);
+             startActivity(in);
+             finish();
+         }
 
 
 
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String emaaill=email.getText().toString();
+                final String emaaill=email.getText().toString();
                 String namee=name.getText().toString();
                 String passs=pass.getText().toString();
                 String conf_passe=confpass.getText().toString();
-                sendSignupData(emaaill,namee,passs,conf_passe);
-                Intent in = new Intent(Signup.this,Login.class);
-                startActivity(in);
-            }
-        });
+                if(TextUtils.isEmpty(emaaill)){
+                    email.setError("email is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(namee)){
+                    name.setError("name is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(passs)){
+                    pass.setError("password is required");
+                    return;
+                }
+                if(passs.length() < 8){
+                    pass.setError("password length must be of greater than or equal to 8 length is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(conf_passe)){
+                    confpass.setError("confirm password is required");
+                    return;
+                }
 
-    }
-    public  void sendSignupData(String email,String name,String pass,String conf_pass){
-        SignupModel signupModel = new SignupModel(email,name,pass,conf_pass);
-        firebaseFirestoredb.collection("Signup")
-                .add(signupModel)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                progressBar.setVisibility(View.VISIBLE);
+                firebaseAuth.createUserWithEmailAndPassword(emaaill,passs).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Toast.makeText(getApplicationContext(),"You have been Signup Sucessfully",Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(getApplicationContext(),"You have encountered an error"+e.getMessage(),Toast.LENGTH_SHORT).show();
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                            Toast.makeText(Signup.this, "You have been register successfully", Toast.LENGTH_SHORT).show();
+                            DocumentReference documentReference = firebaseFirestoredb.collection("Users").document(firebaseUser.getUid());
+                            Map<String,Object> userreginfo = new HashMap<>();
+                            userreginfo.put("Email",email.getText().toString());
+                            userreginfo.put("FullName",name.getText().toString());
+                            userreginfo.put("Pass",pass.getText().toString());
+                            userreginfo.put("ConfPass",confpass.getText().toString());
+                            documentReference.set(userreginfo);
+                            Intent in = new Intent(Signup.this,Navigation.class);
+                            startActivity(in);
+                        }
+                        else { Toast.makeText(Signup.this, "You have been encountered an error"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
                     }
                 });
 
 
 
 
+              //  sendSignupData(emaaill,namee,passs,conf_passe);
+
+            }
+        });
+
+         already_signupp.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent in = new Intent(Signup.this,Login.class);
+                 startActivity(in);
+             }
+         });
 
     }
+//    public  void sendSignupData(final String email, String name, final String pass, String conf_pass){
+//        SignupModel signupModel = new SignupModel(email,name,pass,conf_pass);
+//        firebaseFirestoredb.collection("Signup")
+//                .add(signupModel)
+//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                    @Override
+//                    public void onSuccess(DocumentReference documentReference) {
+//                        Toast.makeText(getApplicationContext(),"You have been Signup Sucessfully",Toast.LENGTH_SHORT).show();
+//                        sendLoginData(email,pass);
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Toast.makeText(getApplicationContext(),"You have encountered an error"+e.getMessage(),Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//
+//
+//
+//
+//
+//
+//
+//    }
+//    public void sendLoginData(String emailsignin, String pass_signin) {
+//        LoginModel loginModel = new LoginModel(emailsignin, pass_signin);
+//        firebaseFirestoredb.collection("SignIn")
+//                .add(loginModel)
+//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                    @Override
+//                    public void onSuccess(DocumentReference documentReference) {
+//                       // Toast.makeText(getApplicationContext(), "You have been Login Sucessfully", Toast.LENGTH_SHORT).show();
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                       // Toast.makeText(getApplicationContext(), "You have encountered an error" + e.getMessage(), Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//
+//    }
 }
